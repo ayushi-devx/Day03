b145403627f0fd02bd02f5b3de95a3bcc4855036
ayushi-devx <innayverma@gmail.com>
2025-06-21 21:29:11 +0530

    allStringProperties

---
+++ string.js
@@ -0,0 +1,33 @@
+var text="hello world";
+var mob="1234567890";
+// console.log(text.at(0),mob.at(1));
+// console.log(text.charAt(0),mob.charAt(0));
+// console.log(text.charCodeAt(0),mob.charCodeAt(0));
+// console.log(text.concat(mob));
+// console.log(text.includes("hello"));
+// console.log(text.indexOf("e"),mob.indexOf("0"));
+// console.log(text.length,mob.length);
+// console.log(text.match(/hello/));
+// console.log(text.repeat(2));
+// console.log(text.replace("hello","dear"));
+// console.log(text.slice(0,11),mob.slice(0,10));
+// console.log(text.substr(0,11),mob.substr(0,10));
+// console.log(text.substring(0,11),mob.substring(0,10));
+// console.log(text.split(" "));
+// console.log(text.split(""));
+
+
+
+console.log(mob.toString());
+console.log(typeof(mob));
+
+
+
+
+
+
+
+
+
+
+
